3095cc2e97206a4c155d9573e76f6956b13b7407
Olga-Pro <oip_@inbox.ru>
2024-06-10 18:02:11 +0300

    create

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+
+
+class RegistrationForm (FlaskForm):
+    username = StringField('Username')
